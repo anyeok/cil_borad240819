d997f8d12b5aed59dadeb2ddd0de16e4503e47ce
anyeok <lanma1621@gmail.com>
2024-08-19 12:45:12 +0900

    1-3 complete

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -14,10 +14,15 @@ public class App {
 
         while (true) {
             System.out.print("명령) ");
-            String command = sc.nextLine();
+            String command = sc.nextLine().trim();
 
             if (command.equals("종료")) {
                 break;
+            } else if(command.equals("등록")){
+                System.out.print("제목 : ");
+                String subject = sc.nextLine().trim();
+                System.out.print("내용 : ");
+                String content = sc.nextLine().trim();
             }
         }
     }
