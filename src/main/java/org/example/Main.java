2fe8636103dfa038035e731a27a0f02f9b53ab62
anyeok <lanma1621@gmail.com>
2024-08-19 12:25:18 +0900

    1-1 complete

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,17 @@
 package org.example;
+import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("HI!!!");
-
+        System.out.println("== 게시판 앱 ==");
+        Scanner sc = new Scanner(System.in);
+        while (true){
+            System.out.print("명령) ");
+            String command = sc.nextLine();
+            if(command.equals("종료")){
+                break;
+            }
+        }
 
     }
 }
